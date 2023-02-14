d08a7bf49cefc732395d69ac01d250dc19ef5d24
Alexey <alexey@yudin6608.ru>
2023-02-14 22:01:24 +0300

    Added code in zadacha3

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//	3 -> Среда 
+//  5 -> Пятница
+
+string[] week = new string[7] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+while (true)
+{
+    Console.WriteLine("Введите число от 1 до 7");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber < 1 || userNumber > 7)
+
+    {
+        Console.WriteLine("Вы ввели не правильное число, попробуйте ещё");
+    }
+    else
+    {
+        int weekIndex = userNumber - 1;
+        Console.WriteLine($"Вы ввели число {userNumber} это день недели {week[weekIndex]}");
+        break;
+    }
+}
