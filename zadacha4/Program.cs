5c414f78b9070b41ac639d5868126eb5487a394e
Alexey <alexey@yudin6608.ru>
2023-02-14 23:10:43 +0300

    Added code in  Zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+System.Console.WriteLine("Введите число N");
+int Number = Convert.ToInt32(Console.ReadLine());
+int count = - Number;
+while(count < Number)
+{
+    System.Console.Write($" {count} ");
+    count ++;
+}
